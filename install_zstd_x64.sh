d71ce06045b5ec2cbb76c695b0ff5696fcdaff9f
Christopher Foo <chris.foo@gmail.com>
2020-09-22 19:53:01 -0400

    Never uninstall libzstd because it makes apt nonfunctional

--- install_zstd_x64.sh
+++ install_zstd_x64.sh
@@ -7,36 +7,26 @@ ZSTD_DEB="zstd_1.4.4+dfsg-3_amd64.deb"
 LIBZSTD_DEB="libzstd1_1.4.4+dfsg-3_amd64.deb"
 LIBZSTD_DEV_DEB="libzstd-dev_1.4.4+dfsg-3_amd64.deb"
 
-function clean_and_exit {
-    echo "*** Failed to install, cleaning up..."
-    sudo dpkg -r zstd libzstd1 libzstd-dev
-    echo "*****"
-    echo "Failed to install zstandard packages."
-    exit 1
-}
+echo "Fixing any existing apt issues..."
+sudo apt install -f -y
+
+echo "Preinstalling old versions..."
+sudo apt install -y zstd libzstd1 libzstd-dev
 
 for DEB in ${ZSTD_DEB} ${LIBZSTD_DEB} ${LIBZSTD_DEV_DEB}; do
-    echo "Installing $DEB ..."
+    echo "Downloading $DEB ..."
 
     curl ${CURL_OPTS} \
         --output $DEB \
         https://warriorhq.archiveteam.org/downloads/wget-at/$DEB
-
-    set +e
-    sudo dpkg -i $DEB
-    EXIT_STATUS=$?
-    set -e
-
-    if [ $EXIT_STATUS -ne 0 ]; then
-        clean_and_exit
-    fi
 done
 
-set +e
-sudo apt install -f
-EXIT_STATUS=$?
-set -e
+echo "Installing new versions..."
 
-if [ $EXIT_STATUS -ne 0 ]; then
-    clean_and_exit
-fi
+set +e
+sudo dpkg -i ${ZSTD_DEB} ${LIBZSTD_DEB} ${LIBZSTD_DEV_DEB}
+set -e
+sudo apt install -f -y
+
+echo "====="
+echo "zstd successfully installed!"
